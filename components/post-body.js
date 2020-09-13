ba564a6d4d9df64a6b65f3bbeff4f7d37cf0954f
UNF0RGIV3N3 <seraf.matteo@gmail.com>
2020-09-13 17:29:07 +0200

    Test DFP

--- components/post-body.js
+++ components/post-body.js
@@ -1,11 +1,12 @@
 import styles from './post-body.module.css'
 import LazyLoad from 'react-lazyload'
 import parse from 'html-react-parser'
+import { DFPSlotsProvider, AdSlot } from 'react-dfp';
 
 export default function PostBody({ content }) {
   return (
     <div className="max-w-2xl mx-auto">
-      <div className={styles.content} >{parse(content, {replace: replaceMedia})}</div>
+      <div className={styles.content + " post__content"} >{parse(content, {replace: replaceMedia})}</div>
     </div>
   )
 }
@@ -20,12 +21,24 @@ const getImage = node => {
     }
   }
 };
-
+var count_p = 0;
 const replaceMedia = node => {
   const image = getImage(node);
   if (image != null) {
     let alt = image.attribs.src.split('/')
     alt = alt[alt.length -1].split('.')[0].replace(/-/g, " ").replace(/[0-9]/g, "").replace(/ x/g, "").replace(/ x /g, "").replace(/_/g, " ").trim()
     return <LazyLoad><img src={image.attribs.src} srcSet={image.attribs.srcset} alt={alt} width={image.attribs.width}/></LazyLoad>;
+  }
+  if (node.name === 'p'){
+    count_p++;
+    if ([3, 9, 12, 15].includes(count_p)) {
+     return (
+        <DFPSlotsProvider dfpNetworkId="9528481">
+          <div className="BOX_TOP-2">
+            <AdSlot sizes={[[300,600],[300,400],[300,250],[320,480],[320,50],'fluid']} adUnit="HALFPAGE_IFOOD.IT" />
+          </div>
+        </DFPSlotsProvider>
+     )
+    }
   }
 };
--- components/post-header.js
+++ components/post-header.js
@@ -3,11 +3,7 @@ import Date from '../components/date'
 import CoverImage from '../components/cover-image'
 import PostTitle from '../components/post-title'
 import Categories from '../components/categories'
-import dynamic from 'next/dynamic'
-
-const DynamicTwitch = dynamic(() => import("../components/twitch"), {
-  ssr: false,
-});
+import { DFPSlotsProvider, AdSlot } from 'react-dfp';
 
 
 export default function PostHeader({
@@ -23,8 +19,12 @@ export default function PostHeader({
       <div className="hidden md:block md:mb-12">
         <Avatar author={author} />
       </div>
-      <DynamicTwitch></DynamicTwitch>
       <div className="mb-8 md:mb-16 sm:mx-0">
+        <DFPSlotsProvider dfpNetworkId="9528481">
+          <div className="BOX_TOP-2">
+            <AdSlot sizes={[[300,250],[336,280],[320,480],[300,600],'fluid']} adUnit="BOX_TOP_IFOOD.IT" />
+          </div>
+        </DFPSlotsProvider>
         <CoverImage title={title} coverImage={coverImage} />
       </div>
       <div className="max-w-2xl mx-auto">
