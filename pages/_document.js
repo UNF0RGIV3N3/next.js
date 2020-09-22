0a4834803ada88f2ed826d66325fbfbc9e88a423
Matteo <matteo.serafini@netaddiction.it>
2020-09-22 17:26:42 +0200

    Quantcast

--- pages/_document.js
+++ pages/_document.js
@@ -6,8 +6,6 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head />
         <link rel="preconnect" href="https://www.ifood.it"/>
-        <link rel="preconnect" href="https://cdn.iubenda.com"/>
-
         <link rel="preload" href="https://securepubads.g.doubleclick.net/tag/js/gpt.js" as="script"/>
         <script
           dangerouslySetInnerHTML={{
@@ -20,15 +18,165 @@ export default class MyDocument extends Document {
         `
           }}
         />
-        <script
+        <script type="text/javascript" async="true"
         dangerouslySetInnerHTML={{
-            __html: `var _iub = _iub || [];
-            _iub.csConfiguration = {"whitelabel":false,"lang":"it","siteId":2010922,"enableCMP":true,"googleAdditionalConsentMode":true,"isTCFConsentGlobal":false,"cookiePolicyId":89564238, "banner":{ "acceptButtonDisplay":true,"customizeButtonDisplay":true,"position":"float-top-center" }};
-        `
+            __html: `<script type="text/javascript" async=true>
+            (function() {
+              var host = window.location.hostname;
+              var element = document.createElement('script');
+              var firstScript = document.getElementsByTagName('script')[0];
+              var url = 'https://quantcast.mgr.consensu.org'
+                .concat('/choice/', 'TVsLuYynpMUH0', '/', host, '/choice.js')
+              var uspTries = 0;
+              var uspTriesLimit = 3;
+              element.async = true;
+              element.type = 'text/javascript';
+              element.src = url;
+
+              firstScript.parentNode.insertBefore(element, firstScript);
+
+              function makeStub() {
+                var TCF_LOCATOR_NAME = '__tcfapiLocator';
+                var queue = [];
+                var win = window;
+                var cmpFrame;
+
+                function addFrame() {
+                  var doc = win.document;
+                  var otherCMP = !!(win.frames[TCF_LOCATOR_NAME]);
+
+                  if (!otherCMP) {
+                    if (doc.body) {
+                      var iframe = doc.createElement('iframe');
+
+                      iframe.style.cssText = 'display:none';
+                      iframe.name = TCF_LOCATOR_NAME;
+                      doc.body.appendChild(iframe);
+                    } else {
+                      setTimeout(addFrame, 5);
+                    }
+                  }
+                  return !otherCMP;
+                }
+
+                function tcfAPIHandler() {
+                  var gdprApplies;
+                  var args = arguments;
+
+                  if (!args.length) {
+                    return queue;
+                  } else if (args[0] === 'setGdprApplies') {
+                    if (
+                      args.length > 3 &&
+                      args[2] === 2 &&
+                      typeof args[3] === 'boolean'
+                    ) {
+                      gdprApplies = args[3];
+                      if (typeof args[2] === 'function') {
+                        args[2]('set', true);
+                      }
+                    }
+                  } else if (args[0] === 'ping') {
+                    var retr = {
+                      gdprApplies: gdprApplies,
+                      cmpLoaded: false,
+                      cmpStatus: 'stub'
+                    };
+
+                    if (typeof args[2] === 'function') {
+                      args[2](retr);
+                    }
+                  } else {
+                    queue.push(args);
+                  }
+                }
+
+                function postMessageEventHandler(event) {
+                  var msgIsString = typeof event.data === 'string';
+                  var json = {};
+
+                  try {
+                    if (msgIsString) {
+                      json = JSON.parse(event.data);
+                    } else {
+                      json = event.data;
+                    }
+                  } catch (ignore) {}
+
+                  var payload = json.__tcfapiCall;
+
+                  if (payload) {
+                    window.__tcfapi(
+                      payload.command,
+                      payload.version,
+                      function(retValue, success) {
+                        var returnMsg = {
+                          __tcfapiReturn: {
+                            returnValue: retValue,
+                            success: success,
+                            callId: payload.callId
+                          }
+                        };
+                        if (msgIsString) {
+                          returnMsg = JSON.stringify(returnMsg);
+                        }
+                        event.source.postMessage(returnMsg, '*');
+                      },
+                      payload.parameter
+                    );
+                  }
+                }
+
+                while (win) {
+                  try {
+                    if (win.frames[TCF_LOCATOR_NAME]) {
+                      cmpFrame = win;
+                      break;
+                    }
+                  } catch (ignore) {}
+
+                  if (win === window.top) {
+                    break;
+                  }
+                  win = win.parent;
+                }
+                if (!cmpFrame) {
+                  addFrame();
+                  win.__tcfapi = tcfAPIHandler;
+                  win.addEventListener('message', postMessageEventHandler, false);
+                }
+              };
+
+              makeStub();
+
+              var uspStubFunction = function() {
+                var arg = arguments;
+                if (typeof window.__uspapi !== uspStubFunction) {
+                  setTimeout(function() {
+                    if (typeof window.__uspapi !== 'undefined') {
+                      window.__uspapi.apply(window.__uspapi, arg);
+                    }
+                  }, 500);
+                }
+              };
+
+              var checkIfUspIsReady = function() {
+                uspTries++;
+                if (window.__uspapi === uspStubFunction && uspTries < uspTriesLimit) {
+                  console.warn('USP is not accessible');
+                } else {
+                  clearInterval(uspInterval);
+                }
+              };
+
+              if (typeof window.__uspapi === 'undefined') {
+                window.__uspapi = uspStubFunction;
+                var uspInterval = setInterval(checkIfUspIsReady, 6000);
+              }
+            })();
+            `
         }}
         />
-        <script type="text/javascript" src="//cdn.iubenda.com/cs/tcf/stub-v2.js" defer></script>
-        <script type="text/javascript" src="//cdn.iubenda.com/cs/iubenda_cs.js" charset="UTF-8" defer></script>
         <body>
           <Main />
           <NextScript />
